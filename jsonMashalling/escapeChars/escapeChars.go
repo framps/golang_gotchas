4d0a54e43336427bb810174584860b8e501ec048
framp <framp@linux-tips-and-tricks.de>
2017-10-17 21:27:08 +0200

    Encode of struct inserted

--- jsonMashalling/escapeChars/escapeChars.go
+++ jsonMashalling/escapeChars/escapeChars.go
@@ -20,35 +20,43 @@ func panicIfError(err error) {
 
 func main() {
 
-	var simpleInterfaceStruct interface{}
-	var simpleStruct struct {
+	type simpleInterfaceType interface{}
+	type simpleStruct struct {
 		Value string
 	}
 
+	var simpleStructVariable simpleStruct = simpleStruct{Value: "Fo\"o\tTAB"}
+	var simpleInterfaceVariable simpleInterfaceType
+
 	var simpleStructBlob = []byte(`{ "Value": "Fo\"o\tTAB", "Null" : null }`)
 
-	err := json.Unmarshal(simpleStructBlob, &simpleInterfaceStruct)
+	err := json.Unmarshal(simpleStructBlob, &simpleInterfaceVariable)
 	panicIfError(err)
-	m := simpleInterfaceStruct.(map[string]interface{})
+	m := simpleInterfaceVariable.(map[string]interface{})
 	v := m["Value"].(string)
 	n := m["Null"]
-	fmt.Printf("*** Unmarshaled into interface ***\n%s\n---\n%#v\n---\nValue: '%s' - Null: %#v\n---\n", simpleStructBlob, simpleInterfaceStruct, v, n)
+	fmt.Printf("*** Blob unmarshaled into interface ***\n%s\n---\n%#v\n---\nValue: '%s' - Null: %#v\n---\n", simpleStructBlob, simpleInterfaceVariable, v, n)
 
-	err = json.Unmarshal(simpleStructBlob, &simpleStruct)
+	err = json.Unmarshal(simpleStructBlob, &simpleStructVariable)
 	panicIfError(err)
-	fmt.Printf("*** Unmarshaled into struct ***\n%s\n---\n%#v\n---\nValue: '%s'\n---\n", simpleStructBlob, simpleStruct, simpleStruct.Value)
+	fmt.Printf("*** Blob unmarshaled into struct ***\n%s\n---\n%#v\n---\nValue: '%s'\n---\n", simpleStructBlob, simpleStructVariable, simpleStructVariable.Value)
 
-	blob, err := json.Marshal(simpleInterfaceStruct)
+	blob, err := json.Marshal(simpleInterfaceVariable)
 	panicIfError(err)
-	fmt.Printf("*** Marshaled from interface ***\n%#v\n---\n%s\n---\n", simpleInterfaceStruct, blob)
+	fmt.Printf("*** Marshaled from interface ***\n%#v\n---\n%s\n---\n", simpleInterfaceVariable, blob)
 
-	simpleStruct.Value = "Some\"Value\t followed by NL\nand next line"
+	simpleStructVariable.Value = "Some\"Value\t followed by NL\nand next line"
 
-	blob, err = json.Marshal(simpleStruct)
+	blob, err = json.Marshal(simpleStructVariable)
 	panicIfError(err)
-	fmt.Printf("*** Marshaled from struct ***\n%#v\n---\n%s\n---\n", simpleStruct, blob)
+	fmt.Printf("*** Marshaled from struct ***\n%#v\n---\n%s\n---\n", simpleStructVariable, blob)
 
 	b := new(bytes.Buffer)
-	json.NewEncoder(b).Encode(simpleStruct.Value)
-	fmt.Printf("*** Encoded ***\n%#s\n---\nWith DoubleQuotes: '%s'\n---\nWithout DoubleQuotes: '%s'\n", simpleStruct.Value, b, b.String()[1:b.Len()-2])
+	json.NewEncoder(b).Encode(simpleStructVariable.Value)
+	fmt.Printf("*** Encoded struct value ***\n%#s\n---\n'%s'\n", simpleStructVariable.Value, b)
+
+	b = new(bytes.Buffer)
+	json.NewEncoder(b).Encode(simpleStructVariable)
+	fmt.Printf("*** Encoded struct ***\n%#s\n---\n'%s'\n", simpleStructVariable.Value, b)
+
 }
